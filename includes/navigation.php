0f0fd4275ef92e3abe3e5c2f3fd05862425ac1a9
DapperBlondie <alirezagharib110@gmail.com>
2021-07-02 12:09:23 +0430

    refactoring applied to index.html

---
+++ includes/navigation.php
@@ -0,0 +1,47 @@
+<?php
+
+?>
+
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">BloggingSystem</a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+
+                <?php
+                include "./includes/db.php";
+                $category_query = "SELECT * FROM category";
+                $category_result_set = mysqli_query($connection, $category_query);
+                while ($row = mysqli_fetch_assoc($category_result_set))
+                {
+                    echo "<li><a href=\"#\">{$row["cat_title"]}</a></li>";
+                }
+
+                ?>
+               <!-- <li>
+                    <a href="#">About</a>
+                </li>
+                <li>
+                    <a href="#">Services</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li> -->
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+</nav>
+
+<?php ?>
